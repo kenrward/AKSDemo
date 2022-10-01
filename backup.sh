7f9b654ed445ce93d6b6e8ed2b3ed2c7921dc37e
Ken Ward <kenrward@gmail.com>
2022-10-01 19:45:03 -0400

    sh scripts

---
+++ backup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -e
+
+DATETIME=`date +"%Y-%m-%d-%H-%M-%S"`
+
+FILENAME=${BACKUP_FILENAME}
+MONGO_HOST=${MONGODB_HOST:=$MONGODB_PORT_27017_TCP_ADDR}
+MONGO_DB=${MONGODB_DB}
+MONGO_URI=${MONGO_URI}
+
+BACKUP_NAME="$FILENAME-$MONGO_DB-$DATETIME"
+BACKUP_FOLDER="/tmp/$BACKUP_NAME/"
+
+make_backup() {
+
+  mkdir "$BACKUP_FOLDER"
+
+  if [[ -n "$MONGO_URI" ]]; then
+    mongodump --uri $MONGO_URI -o $BACKUP_FOLDER
+  else
+    # if 'MONGO_DB' is empty then backup all databases
+    if [[ -n "$MONGO_DB" ]]; then
+      mongodump -h $MONGO_HOST -d $MONGO_DB -o $BACKUP_FOLDER
+    else
+      mongodump -h $MONGO_HOST -o $BACKUP_FOLDER
+    fi
+  fi
+
+  tar -zcvf $BACKUP_NAME.tgz -C /tmp/ $BACKUP_NAME
+}
+
+AZURE_CONTAINER=${AZURE_CONTAINER}
+AZURE_STORAGE_ACCOUNT=${AZURE_STORAGE_ACCOUNT}
+AZURE_STORAGE_KEY=${AZURE_STORAGE_KEY}
+AZURE_ENDPOINT_SUFFIX=${AZURE_ENDPOINT_SUFFIX:-blob.core.windows.net}
+
+AZURE_STORAGE_CONNECTION_STRING="DefaultEndpointsProtocol=https;AccountName=$AZURE_STORAGE_ACCOUNT;AccountKey=$AZURE_STORAGE_KEY;EndpointSuffix=$AZURE_ENDPOINT_SUFFIX"
+
+upload_backup() {
+
+  # Create container if it doesn't exist yet (upload fails otherwise)
+  set +e
+  grep -wq $AZURE_CONTAINER <<< $(az storage container list --connection-string $AZURE_STORAGE_CONNECTION_STRING)
+  CONTAINER_EXISTS=$?
+  set -e
+  if [ "$CONTAINER_EXISTS" -gt "0" ]; then
+    az storage container create -n $AZURE_CONTAINER --connection-string $AZURE_STORAGE_CONNECTION_STRING
+  fi
+
+  az storage blob upload -f $BACKUP_NAME.tgz -n $BACKUP_NAME.tgz -c $AZURE_CONTAINER --connection-string $AZURE_STORAGE_CONNECTION_STRING
+
+}
+
+make_backup
+upload_backup
---
+++ installMongo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org=4.2.18 mongodb-org-server=4.2.18 mongodb-org-shell=4.2.18 mongodb-org-mongos=4.2.18 mongodb-org-tools=4.2.18 --allow-unauthenticated
+sudo systemctl start mongod
+# sudo systemctl status mongod
+   
